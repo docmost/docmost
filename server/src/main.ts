11018ea976220fd973ecbed97cca2728b89acb83
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-08-03 17:29:50 +0100

    switch to fastify

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      ignoreTrailingSlash: true,
+      ignoreDuplicateSlashes: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
