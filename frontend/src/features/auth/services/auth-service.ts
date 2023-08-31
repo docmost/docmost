b8f6c17cf096fed95490195c890af9f406bc4bf7
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-08-31 12:28:44 +0100

    fix type

--- frontend/src/features/auth/services/auth-service.ts
+++ frontend/src/features/auth/services/auth-service.ts
@@ -1,12 +1,12 @@
 import api from "@/lib/api-client";
-import { ILogin, IRegister, ITokenResponse, ITokens } from "@/features/auth/types/auth.types";
+import { ILogin, IRegister, ITokenResponse } from "@/features/auth/types/auth.types";
 
 export async function login(data: ILogin): Promise<ITokenResponse>{
-  const req = await api.post<ITokens>("/auth/login", data);
+  const req = await api.post<ITokenResponse>("/auth/login", data);
   return req.data as ITokenResponse;
 }
 
 export async function register(data: IRegister): Promise<ITokenResponse>{
-  const req = await api.post<ITokens>("/auth/register", data);
+  const req = await api.post<ITokenResponse>("/auth/register", data);
   return req.data as ITokenResponse;
 }
