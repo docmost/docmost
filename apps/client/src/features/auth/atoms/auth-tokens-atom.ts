14197d7365e8ee36d6b0268346b1562d8edf2095
Philipinho <16838612+Philipinho@users.noreply.github.com>
2024-07-12 14:52:09 +0100

    add cookie duration

--- apps/client/src/features/auth/atoms/auth-tokens-atom.ts
+++ apps/client/src/features/auth/atoms/auth-tokens-atom.ts
@@ -1,14 +1,17 @@
 import Cookies from "js-cookie";
 import { createJSONStorage, atomWithStorage } from "jotai/utils";
-import { ITokens } from '../types/auth.types';
-
+import { ITokens } from "../types/auth.types";
 
 const cookieStorage = createJSONStorage<ITokens>(() => {
   return {
     getItem: () => Cookies.get("authTokens"),
-    setItem: (key, value) => Cookies.set(key, value),
+    setItem: (key, value) => Cookies.set(key, value, { expires: 30 }),
     removeItem: (key) => Cookies.remove(key),
   };
 });
 
-export const authTokensAtom = atomWithStorage<ITokens | null>("authTokens", null, cookieStorage);
+export const authTokensAtom = atomWithStorage<ITokens | null>(
+  "authTokens",
+  null,
+  cookieStorage,
+);
