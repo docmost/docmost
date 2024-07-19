e39a9291b4c955a85045f56f793901669c656e7a
Philipinho <16838612+Philipinho@users.noreply.github.com>
2024-07-19 23:47:46 +0100

    extract page title from h1 heading

--- apps/server/src/integrations/import/import.service.ts
+++ apps/server/src/integrations/import/import.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { PageRepo } from '@docmost/db/repos/page/page.repo';
 import { MultipartFile } from '@fastify/multipart';
 import { sanitize } from 'sanitize-filename-ts';
@@ -27,55 +27,42 @@ export class ImportService {
     workspaceId: string,
   ): Promise<void> {
     const file = await filePromise;
-    const buffer = await file.toBuffer();
-    const sanitizedFilename = sanitize(file.filename);
-    let fileName = sanitizedFilename.slice(0, 255);
+    const fileBuffer = await file.toBuffer();
+    const fileName = sanitize(file.filename).slice(0, 255).split('.')[0];
     const fileExtension = path.extname(file.filename).toLowerCase();
     const fileMimeType = file.mimetype;
+    const fileContent = fileBuffer.toString();
 
-    const fileContent = buffer.toString();
-
-    //TODO: get the page title
-    // if the first node a h1 heading, use it as page title
-    // else,  use the file name but without the file extension
-
-    let prosemirrorJson = null;
+    let prosemirrorState = null;
     let createdPage = null;
 
     if (fileExtension.endsWith('.md') && fileMimeType === 'text/markdown') {
-      fileName = fileName.split('.md')[0];
-      prosemirrorJson = await this.processMarkdown(fileContent);
+      prosemirrorState = await this.processMarkdown(fileContent);
     }
 
     if (fileExtension.endsWith('.html') && fileMimeType === 'text/html') {
-      fileName = fileName.split('.html')[0];
-      prosemirrorJson = await this.processHTML(fileContent);
+      prosemirrorState = await this.processHTML(fileContent);
     }
 
+    if (!prosemirrorState) {
+      const message = 'Unsupported file format or mime type';
+      this.logger.error(message);
+      throw new BadRequestException(message);
+    }
+
+    const { title, prosemirrorJson } =
+      this.extractTitleAndRemoveHeading(prosemirrorState);
+
+    const pageTitle = title || fileName;
+
     if (prosemirrorJson) {
-      // Imported pages will go to the bottom of root node
-      const lastPage = await this.db
-        .selectFrom('pages')
-        .select(['id', 'position'])
-        .where('spaceId', '=', spaceId)
-        .orderBy('position', 'desc')
-        .limit(1)
-        .where('parentPageId', 'is', null)
-        .executeTakeFirst();
-
-      let pagePosition: string;
-
-      // if no existing page, make this the first
-      if (lastPage) {
-        pagePosition = generateJitteredKeyBetween(lastPage.position, null);
-      } else {
-        pagePosition = generateJitteredKeyBetween(null, null);
-      }
-
       try {
+        // Imported pages will go to the bottom of the space root
+        const pagePosition = await this.getNewPagePosition(spaceId);
+
         createdPage = await this.pageRepo.insertPage({
           slugId: generateSlugId(),
-          title: fileName,
+          title: pageTitle,
           content: prosemirrorJson,
           position: pagePosition,
           spaceId: spaceId,
@@ -84,8 +71,9 @@ export class ImportService {
           lastUpdatedById: userId,
         });
       } catch (err) {
-        // use logger
-        this.logger.error('failed to create imported page', err);
+        const message = 'Failed to create imported page';
+        this.logger.error(message, err);
+        throw new BadRequestException(message);
       }
     }
 
@@ -94,7 +82,6 @@ export class ImportService {
 
   async processMarkdown(markdownInput: string): Promise<any> {
     // turn markdown to html
-    // TODO: use marked extensions to enhance it
     const html = await marked.parse(markdownInput);
     return await this.processHTML(html);
   }
@@ -102,5 +89,39 @@ export class ImportService {
   async processHTML(htmlInput: string): Promise<any> {
     // turn html to prosemirror state
     return htmlToJson(transformHTML(htmlInput));
+  }
+
+  extractTitleAndRemoveHeading(prosemirrorState: any) {
+    let title = null;
+
+    if (
+      prosemirrorState?.content?.length > 0 &&
+      prosemirrorState.content[0].type === 'heading' &&
+      prosemirrorState.content[0].attrs?.level === 1
+    ) {
+      title = prosemirrorState.content[0].content[0].text;
+
+      // remove h1 header node from state
+      prosemirrorState.content.shift();
+    }
+
+    return { title, prosemirrorJson: prosemirrorState };
+  }
+
+  async getNewPagePosition(spaceId: string): Promise<string> {
+    const lastPage = await this.db
+      .selectFrom('pages')
+      .select(['id', 'position'])
+      .where('spaceId', '=', spaceId)
+      .orderBy('position', 'desc')
+      .limit(1)
+      .where('parentPageId', 'is', null)
+      .executeTakeFirst();
+
+    if (lastPage) {
+      return generateJitteredKeyBetween(lastPage.position, null);
+    } else {
+      return generateJitteredKeyBetween(null, null);
+    }
   }
 }
