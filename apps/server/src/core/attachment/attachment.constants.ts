8af2d4e8cf3358426d2f3f0312562e2530f421e9
Philipinho <16838612+Philipinho@users.noreply.github.com>
2024-09-02 16:39:07 +0100

    file content-disposition

--- apps/server/src/core/attachment/attachment.constants.ts
+++ apps/server/src/core/attachment/attachment.constants.ts
@@ -5,10 +5,10 @@ export enum AttachmentType {
   File = 'file',
 }
 
-export const validImageExtensions = ['.jpg', '.png', '.jpeg', 'gif'];
+export const validImageExtensions = ['.jpg', '.png', '.jpeg'];
 export const MAX_AVATAR_SIZE = '5MB';
 
-export const InlineFileExtensions = [
+export const inlineFileExtensions = [
   '.jpg',
   '.png',
   '.jpeg',
