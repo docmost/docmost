f380109eff162254994337ee6e3b1b0d8a6bad18
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-01-05 15:09:55 +0000

    feat: handle internal link paste

--- apps/client/src/features/editor/components/common/editor-paste-handler.tsx
+++ apps/client/src/features/editor/components/common/editor-paste-handler.tsx
@@ -2,12 +2,35 @@ import type { EditorView } from "@tiptap/pm/view";
 import { uploadImageAction } from "@/features/editor/components/image/upload-image-action.tsx";
 import { uploadVideoAction } from "@/features/editor/components/video/upload-video-action.tsx";
 import { uploadAttachmentAction } from "../attachment/upload-attachment-action";
+import { createMentionAction } from "@/features/editor/components/link/internal-link-paste.ts";
+import { Slice } from "@tiptap/pm/model";
+import { INTERNAL_LINK_REGEX } from "@/lib/constants.ts";
 
-export const handleFilePaste = (
+export const handlePaste = (
   view: EditorView,
   event: ClipboardEvent,
   pageId: string,
+  creatorId?: string,
 ) => {
+  const clipboardData = event.clipboardData.getData("text/plain");
+
+  if (INTERNAL_LINK_REGEX.test(clipboardData)) {
+    // we have to do this validation here to allow the default link extension to takeover if needs be
+    event.preventDefault();
+    const url = clipboardData.trim();
+    const { from: pos, empty } = view.state.selection;
+    const match = INTERNAL_LINK_REGEX.exec(url);
+
+    // pasted link must be from the same workspace/domain and must not be on a selection
+    if (!empty || match[2] !== window.location.host) {
+      // allow the default link extension to handle this
+      return false;
+    }
+
+    createMentionAction(url, view, pos, creatorId);
+    return true;
+  }
+
   if (event.clipboardData?.files.length) {
     event.preventDefault();
     const [file] = Array.from(event.clipboardData.files);
