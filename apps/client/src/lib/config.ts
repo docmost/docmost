d9de5f7f1a5b6ab799ff82a679263068b1728f98
sasanqu_a <sazanka0316@gmail.com>
2025-02-22 21:17:15 +0900

    feat: app_name get by env

--- apps/client/src/lib/config.ts
+++ apps/client/src/lib/config.ts
@@ -1,62 +1,60 @@
-import bytes from "bytes";
+import bytes from 'bytes';
 
 declare global {
-  interface Window {
-    CONFIG?: Record<string, string>;
-  }
+	interface Window {
+		CONFIG?: Record<string, string>;
+	}
 }
 
 export function getAppName(): string {
-  return "Docmost";
+	return getConfigValue('APP_NAME', 'Docmost');
 }
 
 export function getAppUrl(): string {
-  return `${window.location.protocol}//${window.location.host}`;
+	return `${window.location.protocol}//${window.location.host}`;
 }
 
 export function getBackendUrl(): string {
-  return getAppUrl() + "/api";
+	return getAppUrl() + '/api';
 }
 
 export function getCollaborationUrl(): string {
-  const COLLAB_PATH = "/collab";
+	const COLLAB_PATH = '/collab';
 
-  let url = getAppUrl();
-  if (import.meta.env.DEV) {
-    url = process.env.APP_URL;
-  }
+	let url = getAppUrl();
+	if (import.meta.env.DEV) {
+		url = process.env.APP_URL;
+	}
 
-  const wsProtocol = url.startsWith("https") ? "wss" : "ws";
-  return `${wsProtocol}://${url.split("://")[1]}${COLLAB_PATH}`;
+	const wsProtocol = url.startsWith('https') ? 'wss' : 'ws';
+	return `${wsProtocol}://${url.split('://')[1]}${COLLAB_PATH}`;
 }
 
 export function getAvatarUrl(avatarUrl: string) {
-  if (!avatarUrl) return null;
-  if (avatarUrl?.startsWith("http")) return avatarUrl;
+	if (!avatarUrl) return null;
+	if (avatarUrl?.startsWith('http')) return avatarUrl;
 
-  return getBackendUrl() + "/attachments/img/avatar/" + avatarUrl;
+	return getBackendUrl() + '/attachments/img/avatar/' + avatarUrl;
 }
 
 export function getSpaceUrl(spaceSlug: string) {
-  return "/s/" + spaceSlug;
+	return '/s/' + spaceSlug;
 }
 
 export function getFileUrl(src: string) {
-  return src?.startsWith("/files/") ? getBackendUrl() + src : src;
+	return src?.startsWith('/files/') ? getBackendUrl() + src : src;
 }
 
 export function getFileUploadSizeLimit() {
-  const limit = getConfigValue("FILE_UPLOAD_SIZE_LIMIT", "50mb");
-  return bytes(limit);
+	const limit = getConfigValue('FILE_UPLOAD_SIZE_LIMIT', '50mb');
+	return bytes(limit);
 }
 
 export function getDrawioUrl() {
-  return getConfigValue("DRAWIO_URL", "https://embed.diagrams.net");
+	return getConfigValue('DRAWIO_URL', 'https://embed.diagrams.net');
 }
 
 function getConfigValue(key: string, defaultValue: string = undefined): string {
-  const rawValue = import.meta.env.DEV
-    ? process?.env?.[key]
-    : window?.CONFIG?.[key];
-  return rawValue ?? defaultValue;
+	const rawValue = import.meta.env.DEV ? process?.env?.[key] : window?.CONFIG?.[key];
+	return rawValue ?? defaultValue;
 }
