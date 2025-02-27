3cfb17bb628365e331958b83b6fc22e71cb64d4c
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-02-27 14:44:28 +0000

    fix sentry

--- apps/server/src/common/sentry/instrument.ts
+++ apps/server/src/common/sentry/instrument.ts
@@ -4,13 +4,9 @@ import { envPath } from '../helpers';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: envPath });
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      nodeProfilingIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0,
-  });
-}
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
