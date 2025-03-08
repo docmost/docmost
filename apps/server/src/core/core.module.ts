30acc6676a6c9db3911f62d8d19e46f9da4aa92c
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-03-08 19:08:02 +0000

    exclude billing webhook endpoint

--- apps/server/src/core/core.module.ts
+++ apps/server/src/core/core.module.ts
@@ -38,6 +38,7 @@ export class CoreModule implements NestModule {
         { path: 'auth/setup', method: RequestMethod.POST },
         { path: 'health', method: RequestMethod.GET },
         { path: 'health/live', method: RequestMethod.GET },
+        { path: 'billing/stripe/webhook', method: RequestMethod.POST },
       )
       .forRoutes('*');
   }
