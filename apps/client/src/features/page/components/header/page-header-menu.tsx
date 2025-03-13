8f2635f6c6dd0852a8b8fff5d84b85b900275b63
Philip Okugbe <16838612+Philipinho@users.noreply.github.com>
2025-03-13 14:54:18 +0000

    feat: add page stats to page menu (#876)

--- apps/client/src/features/page/components/header/page-header-menu.tsx
+++ apps/client/src/features/page/components/header/page-header-menu.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Group, Menu, Tooltip } from "@mantine/core";
+import { ActionIcon, Group, Menu, Text, Tooltip } from "@mantine/core";
 import {
   IconAlignRight2,
   IconArrowsHorizontal,
@@ -26,9 +26,13 @@ import { extractPageSlugId } from "@/lib";
 import { treeApiAtom } from "@/features/page/tree/atoms/tree-api-atom.ts";
 import { useDeletePageModal } from "@/features/page/hooks/use-delete-page-modal.tsx";
 import { PageWidthToggle } from "@/features/user/components/page-width-pref.tsx";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import ExportModal from "@/components/common/export-modal";
-import { yjsConnectionStatusAtom } from "@/features/editor/atoms/editor-atoms.ts";
+import {
+  pageEditorAtom,
+  yjsConnectionStatusAtom,
+} from "@/features/editor/atoms/editor-atoms.ts";
+import { formattedDate, timeAgo } from "@/lib/time.ts";
 import { ViewHeadingsToggle } from "@/features/user/components/view-headings";
 import { viewHeadingsAtom } from "@/components/layouts/global/hooks/atoms/sidebar-atom";
 import { userAtom } from "@/features/user/atoms/current-user-atom";
@@ -99,6 +103,7 @@ function PageActionMenu({ readOnly }: PageActionMenuProps) {
   const [tree] = useAtom(treeApiAtom);
   const [exportOpened, { open: openExportModal, close: closeExportModal }] =
     useDisclosure(false);
+  const [pageEditor] = useAtom(pageEditorAtom);
 
   const handleCopyLink = () => {
     const pageUrl =
@@ -128,7 +133,7 @@ function PageActionMenu({ readOnly }: PageActionMenuProps) {
         shadow="xl"
         position="bottom-end"
         offset={20}
-        width={200}
+        width={230}
         withArrow
         arrowPosition="center"
       >
@@ -194,6 +199,41 @@ function PageActionMenu({ readOnly }: PageActionMenuProps) {
               </Menu.Item>
             </>
           )}
+
+          <Menu.Divider />
+
+          <>
+            <Group px="sm" wrap="nowrap" style={{ cursor: "pointer" }}>
+              <Tooltip
+                label={t("Edited by {{name}} {{time}}", {
+                  name: page.lastUpdatedBy.name,
+                  time: timeAgo(page.updatedAt),
+                })}
+                position="left-start"
+              >
+                <div style={{ width: 210 }}>
+                  <Text size="xs" c="dimmed" truncate="end">
+                    {t("Word count: {{wordCount}}", {
+                      wordCount: pageEditor?.storage?.characterCount?.words(),
+                    })}
+                  </Text>
+
+                  <Text size="xs" c="dimmed" lineClamp={1}>
+                    <Trans
+                      defaults="Created by: <b>{{creatorName}}</b>"
+                      values={{ creatorName: page?.creator?.name }}
+                      components={{ b: <Text span fw={500} /> }}
+                    />
+                  </Text>
+                  <Text size="xs" c="dimmed" truncate="end">
+                    {t("Created at: {{time}}", {
+                      time: formattedDate(page.createdAt),
+                    })}
+                  </Text>
+                </div>
+              </Tooltip>
+            </Group>
+          </>
         </Menu.Dropdown>
       </Menu>
 
