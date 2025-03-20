44d2c20b8cb8104c0a9b14fb6efb260b4c842331
sasanqua <sasanqua.dev@gmail.com>
2025-03-20 18:50:21 +0900

    feat: update Error404 component to include app name in page title

--- apps/client/src/components/ui/error-404.tsx
+++ apps/client/src/components/ui/error-404.tsx
@@ -1,28 +1,32 @@
-import { Title, Text, Button, Container, Group } from "@mantine/core";
-import classes from "./error-404.module.css";
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
-import { useTranslation } from "react-i18next";
+import { Title, Text, Button, Container, Group } from '@mantine/core';
+import classes from './error-404.module.css';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import { getAppName } from '@/lib/config';
 
 export function Error404() {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
+	const app_name = getAppName();
 
-  return (
-    <>
-      <Helmet>
-        <title>{t("404 page not found")} - Docmost</title>
-      </Helmet>
-      <Container className={classes.root}>
-        <Title className={classes.title}>{t("404 page not found")}</Title>
-        <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-          {t("Sorry, we can't find the page you are looking for.")}
-        </Text>
-        <Group justify="center">
-          <Button component={Link} to={"/home"} variant="subtle" size="md">
-            {t("Take me back to homepage")}
-          </Button>
-        </Group>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Helmet>
+				<title>
+					{t('404 page not found')} - {app_name}
+				</title>
+			</Helmet>
+			<Container className={classes.root}>
+				<Title className={classes.title}>{t('404 page not found')}</Title>
+				<Text c="dimmed" size="lg" ta="center" className={classes.description}>
+					{t("Sorry, we can't find the page you are looking for.")}
+				</Text>
+				<Group justify="center">
+					<Button component={Link} to={'/home'} variant="subtle" size="md">
+						{t('Take me back to homepage')}
+					</Button>
+				</Group>
+			</Container>
+		</>
+	);
 }
