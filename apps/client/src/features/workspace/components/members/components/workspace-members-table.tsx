1eaba6e77f57c8d8f745336f2b857236ebf9a47a
Philip Okugbe <16838612+Philipinho@users.noreply.github.com>
2025-04-08 13:34:00 +0100

    fix: bug fixes (#1000)
    * sort by groups first
    
    * add scroll area
    
    * fix group members pagination
    
    * move pagination to the right

--- apps/client/src/features/workspace/components/members/components/workspace-members-table.tsx
+++ apps/client/src/features/workspace/components/members/components/workspace-members-table.tsx
@@ -17,7 +17,6 @@ import Paginate from "@/components/common/paginate.tsx";
 import { SearchInput } from "@/components/common/search-input.tsx";
 import NoTableResults from "@/components/common/no-table-results.tsx";
 import { usePaginateAndSearch } from "@/hooks/use-paginate-and-search.tsx";
-import InviteActionMenu from "@/features/workspace/components/members/components/invite-action-menu.tsx";
 import MemberActionMenu from "@/features/workspace/components/members/components/members-action-menu.tsx";
 
 export default function WorkspaceMembersTable() {
