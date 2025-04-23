08f223899a415b5d95b53a0c1d003401778f0dc2
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-04-23 16:07:58 +0100

    cloud trial refactor

--- apps/client/src/ee/billing/components/billing-trial.tsx
+++ apps/client/src/ee/billing/components/billing-trial.tsx
@@ -1,6 +1,7 @@
 import { Alert } from "@mantine/core";
 import { useBillingQuery } from "@/ee/billing/queries/billing-query.ts";
 import useTrial from "@/ee/hooks/use-trial.tsx";
+import { getBillingTrialDays } from '@/lib/config.ts';
 
 export default function BillingTrial() {
   const { data: billing, isLoading } = useBillingQuery();
@@ -15,14 +16,14 @@ export default function BillingTrial() {
       {trialDaysLeft > 0 && !billing && (
         <Alert title="Your Trial is Active 🎉" color="blue" radius="md">
           You have {trialDaysLeft} {trialDaysLeft === 1 ? "day" : "days"} left
-          in your 7-day trial. Please subscribe to a plan before your trial
+          in your {getBillingTrialDays()}-day free trial. Please subscribe to a paid plan before your trial
           ends.
         </Alert>
       )}
 
       {trialDaysLeft === 0 && (
         <Alert title="Your Trial has ended" color="red" radius="md">
-          Your 7-day trial has come to an end. Please subscribe to a plan to
+          Your {getBillingTrialDays()}-day free trial has come to an end. Please subscribe to a paid plan to
           continue using this service.
         </Alert>
       )}
