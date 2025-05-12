c173d219a83a785ce49ee1c1f21f1b144743bb43
sanua356 <sanek.pankratov356@gmail.com>
2025-05-12 09:13:29 +0300

    #1156/chore: add page width toggle for shared pages

--- apps/client/src/pages/share/shared-page.tsx
+++ apps/client/src/pages/share/shared-page.tsx
@@ -7,12 +7,16 @@ import React, { useEffect } from "react";
 import ReadonlyPageEditor from "@/features/editor/readonly-page-editor.tsx";
 import { extractPageSlugId } from "@/lib";
 import { Error404 } from "@/components/ui/error-404.tsx";
+import { useAtom } from "jotai";
+import { shareFullPageWidthAtom } from "@/features/share/atoms/sidebar-atom";
 
 export default function SingleSharedPage() {
   const { t } = useTranslation();
   const { pageSlug } = useParams();
   const { shareId } = useParams();
   const navigate = useNavigate();
+
+  const [isFullWidth] = useAtom(shareFullPageWidthAtom);
 
   const { data, isLoading, isError, error } = useSharePageQuery({
     pageId: extractPageSlugId(pageSlug),
@@ -46,7 +50,7 @@ export default function SingleSharedPage() {
         )}
       </Helmet>
 
-      <Container size={900} p={0}>
+      <Container size={isFullWidth ? "100%" : 900} p={0}>
         <ReadonlyPageEditor
           key={data.page.id}
           title={data.page.title}
