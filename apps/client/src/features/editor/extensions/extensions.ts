f6ad73392e17e56301091755fada0d7d075e98f0
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-06-18 13:26:43 -0700

    fix word counter

--- apps/client/src/features/editor/extensions/extensions.ts
+++ apps/client/src/features/editor/extensions/extensions.ts
@@ -73,6 +73,7 @@ import i18n from "@/i18n.ts";
 import { MarkdownClipboard } from "@/features/editor/extensions/markdown-clipboard.ts";
 import EmojiCommand from "./emoji-command";
 import { CharacterCount } from "@tiptap/extension-character-count";
+import { countWords } from "alfaaz";
 
 const lowlight = createLowlight(common);
 lowlight.register("mermaid", plaintext);
@@ -213,7 +214,9 @@ export const mainExtensions = [
   MarkdownClipboard.configure({
     transformPastedText: true,
   }),
-  CharacterCount
+  CharacterCount.configure({
+    wordCounter: (text) => countWords(text),
+  }),
 ] as any;
 
 type CollabExtensions = (provider: HocuspocusProvider, user: IUser) => any[];
