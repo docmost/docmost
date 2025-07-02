4f1365b3116288796ae869e91a6e13e512288297
fuscodev <stefano.fusco88@gmail.com>
2025-07-02 19:51:04 +0200

    force save content and page history

--- apps/client/src/features/editor/page-editor.tsx
+++ apps/client/src/features/editor/page-editor.tsx
@@ -194,6 +194,11 @@ export default function PageEditor({
     ];
   }, [remoteProvider, currentUser?.user]);
 
+  const debouncedSendSaveCommand = useDebouncedCallback(() => {
+    const payload = 'forceSave';
+    remoteProvider.sendStateless(payload);
+  }, 300);
+
   const editor = useEditor(
     {
       extensions,
@@ -207,6 +212,7 @@ export default function PageEditor({
           keydown: (_view, event) => {
             if ((event.ctrlKey || event.metaKey) && event.code === 'KeyS') {
               event.preventDefault();
+              debouncedSendSaveCommand();
               return true;
             }
             if (["ArrowUp", "ArrowDown", "Enter"].includes(event.key)) {
