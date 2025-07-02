c6b740ef058ccb3dc7007fb9b45f27cd977d8ad0
王野未 <826036140@qq.com>
2025-07-02 14:54:08 +0800

    feat: toggle color scheme i18n

--- apps/client/src/components/theme-toggle.tsx
+++ apps/client/src/components/theme-toggle.tsx
@@ -6,19 +6,22 @@ import {
 } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 import classes from "./theme-toggle.module.css";
+import { useTranslation } from "react-i18next";
 
 export function ThemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme();
 
+  const { t } = useTranslation();
+
   return (
-    <Tooltip label="Toggle Color Scheme">
+    <Tooltip label={t("Toggle Color Scheme")}>
       <ActionIcon
         variant="default"
         onClick={() => {
           setColorScheme(computedColorScheme === "light" ? "dark" : "light");
         }}
-        aria-label="Toggle color scheme"
+        aria-label={t("Toggle Color Scheme")}
       >
         <IconSun className={classes.light} size={18} stroke={1.5} />
         <IconMoon className={classes.dark} size={18} stroke={1.5} />
