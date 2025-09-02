fc51a11ee84f7942686725d133a38014ec49d921
Philipinho <16838612+Philipinho@users.noreply.github.com>
2025-09-01 21:15:17 -0700

    add GIN index

--- apps/server/src/database/migrations/20250901T184612-attachments-search.ts
+++ apps/server/src/database/migrations/20250901T184612-attachments-search.ts
@@ -6,9 +6,21 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('text_content', 'text', (col) => col)
     .addColumn('tsv', sql`tsvector`, (col) => col)
     .execute();
+
+  await db.schema
+    .createIndex('attachments_tsv_idx')
+    .on('pages')
+    .using('GIN')
+    .column('tsv')
+    .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('attachments')
+    .dropIndex('attachments_tsv_idx')
+    .execute();
+
   await db.schema
     .alterTable('attachments')
     .dropColumn('text_content')
