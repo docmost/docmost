9e5d61e766d1d06fe0ec5a913ea4ea3d84f3f264
Eshwar Tangirala <eshwartangirala11@gmail.com>
2025-10-07 17:57:52 -0400

    Adding ability to mark page as home page for space

--- apps/client/src/features/page/components/header/page-header-menu.tsx
+++ apps/client/src/features/page/components/header/page-header-menu.tsx
@@ -14,7 +14,7 @@ import {
   IconTrash,
   IconWifiOff,
 } from "@tabler/icons-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useToggleAside from "@/hooks/use-toggle-aside.tsx";
 import { useAtom } from "jotai";
 import { historyAtoms } from "@/features/page-history/atoms/history-atoms.ts";
@@ -86,20 +86,22 @@ export default function PageHeaderMenu({
       ],
     ],
     []
-  );  
-  
+  );
 
   const handleUpdateHomePage = async () => {
-    const newHomePageValue = isHomePage ? null : pageId
+    const newHomePageValue = isHomePage ? null : pageId;
     const spaceData: Partial<ISpace> = {
       spaceId: spaceId,
       homePageId: newHomePageValue,
     };
-    
+
     await updateSpaceMutation.mutateAsync(spaceData);
     setIsHomePage(!isHomePage);
-    
   };
+
+  useEffect(() => {
+    setIsHomePage(isHome);
+  }, [isHome]);
 
   return (
     <>
