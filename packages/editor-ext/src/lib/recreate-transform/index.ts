f44bb3f1217e88a6ba42a7a06bd8d4cb9742566d
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-01-21 11:45:41 +0000

    V2 - WIP

---
+++ packages/editor-ext/src/lib/recreate-transform/index.ts
@@ -0,0 +1,4 @@
+// https://gitlab.com/mpapp-public/prosemirror-recreate-steps
+// https://github.com/sueddeutsche/prosemirror-recreate-transform
+export { recreateTransform, RecreateTransform } from "./recreateTransform";
+export type { Options } from "./recreateTransform";
