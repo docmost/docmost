25f4b8c2b497c639465ec9595fc9cae96c6aa416
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-02-11 17:47:30 -0800

    fix

--- packages/editor-ext/src/lib/heading/heading.ts
+++ packages/editor-ext/src/lib/heading/heading.ts
@@ -10,6 +10,7 @@ const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
 const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" style="color: forestgreen;" width="18" height="18" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"/></svg>`;
 
 export const Heading = TiptapHeading.extend<TiptapHeadingOptions>({
+  // @ts-ignore
   addProseMirrorPlugins() {
     return [
       new Plugin({
