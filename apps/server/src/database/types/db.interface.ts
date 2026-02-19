873c9630439c04bbdca4fb07dbf6a8b0781bee4a
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-02-19 22:34:07 +0000

    fix db types duplication

--- apps/server/src/database/types/db.interface.ts
+++ apps/server/src/database/types/db.interface.ts
@@ -1,51 +1,6 @@
-import {
-  ApiKeys,
-  Attachments,
-  AuthAccounts,
-  AuthProviders,
-  Backlinks,
-  Billing,
-  Comments,
-  FileTasks,
-  Groups,
-  GroupUsers,
-  Notifications,
-  PageHistory,
-  Pages,
-  Shares,
-  SpaceMembers,
-  Spaces,
-  UserMfa,
-  Users,
-  UserTokens,
-  Watchers,
-  WorkspaceInvitations,
-  Workspaces,
-} from '@docmost/db/types/db';
+import { DB } from '@docmost/db/types/db';
 import { PageEmbeddings } from '@docmost/db/types/embeddings.types';
 
-export interface DbInterface {
-  attachments: Attachments;
-  authAccounts: AuthAccounts;
-  authProviders: AuthProviders;
-  backlinks: Backlinks;
-  billing: Billing;
-  comments: Comments;
-  fileTasks: FileTasks;
-  groups: Groups;
-  groupUsers: GroupUsers;
-  notifications: Notifications;
+export interface DbInterface extends DB {
   pageEmbeddings: PageEmbeddings;
-  pageHistory: PageHistory;
-  pages: Pages;
-  shares: Shares;
-  spaceMembers: SpaceMembers;
-  spaces: Spaces;
-  userMfa: UserMfa;
-  users: Users;
-  userTokens: UserTokens;
-  watchers: Watchers;
-  workspaceInvitations: WorkspaceInvitations;
-  workspaces: Workspaces;
-  apiKeys: ApiKeys;
 }
