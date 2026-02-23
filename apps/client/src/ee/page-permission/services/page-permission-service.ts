a34a6cba6999468b7d37fef78efd720a4d7b428d
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-02-23 03:08:04 +0000

    endpoint refactor

--- apps/client/src/ee/page-permission/services/page-permission-service.ts
+++ apps/client/src/ee/page-permission/services/page-permission-service.ts
@@ -9,29 +9,29 @@ import {
 } from "@/ee/page-permission/types/page-permission.types";
 
 export async function restrictPage(pageId: string): Promise<void> {
-  await api.post("/pages/permissions/restrict", { pageId });
+  await api.post("/pages/restrict", { pageId });
 }
 
 export async function addPagePermission(
   data: IAddPagePermission,
 ): Promise<void> {
-  await api.post("/pages/permissions/add-members", data);
+  await api.post("/pages/add-permission", data);
 }
 
 export async function removePagePermission(
   data: IRemovePagePermission,
 ): Promise<void> {
-  await api.post("/pages/permissions/remove-members", data);
+  await api.post("/pages/remove-permission", data);
 }
 
 export async function updatePagePermissionRole(
   data: IUpdatePagePermissionRole,
 ): Promise<void> {
-  await api.post("/pages/permissions/change-role", data);
+  await api.post("/pages/update-permission", data);
 }
 
 export async function unrestrictPage(pageId: string): Promise<void> {
-  await api.post("/pages/permissions/unrestrict", { pageId });
+  await api.post("/pages/remove-restriction", { pageId });
 }
 
 export async function getPagePermissions(
@@ -39,7 +39,7 @@ export async function getPagePermissions(
   params?: QueryParams,
 ): Promise<IPagination<IPagePermissionMember>> {
   const req = await api.post<IPagination<IPagePermissionMember>>(
-    "/pages/permissions/members",
+    "/pages/permissions",
     { pageId, ...params },
   );
   return req.data;
@@ -48,7 +48,7 @@ export async function getPagePermissions(
 export async function getPageRestrictionInfo(
   pageId: string,
 ): Promise<IPageRestrictionInfo> {
-  const req = await api.post<IPageRestrictionInfo>("/pages/permissions/info", {
+  const req = await api.post<IPageRestrictionInfo>("/pages/permission-info", {
     pageId,
   });
   return req.data;
--- apps/server/src/core/page/page.module.ts
+++ apps/server/src/core/page/page.module.ts
@@ -3,21 +3,14 @@ import { PageService } from './services/page.service';
 import { PageController } from './page.controller';
 import { PageHistoryService } from './services/page-history.service';
 import { TrashCleanupService } from './services/trash-cleanup.service';
-import { PagePermissionService } from './services/page-permission.service';
-import { PagePermissionController } from './page-permission.controller';
 import { StorageModule } from '../../integrations/storage/storage.module';
 import { CollaborationModule } from '../../collaboration/collaboration.module';
 import { WatcherModule } from '../watcher/watcher.module';
 
 @Module({
-  controllers: [PageController, PagePermissionController],
-  providers: [
-    PageService,
-    PageHistoryService,
-    TrashCleanupService,
-    PagePermissionService,
-  ],
-  exports: [PageService, PageHistoryService, PagePermissionService],
+  controllers: [PageController],
+  providers: [PageService, PageHistoryService, TrashCleanupService],
+  exports: [PageService, PageHistoryService],
   imports: [StorageModule, CollaborationModule, WatcherModule],
 })
 export class PageModule {}
