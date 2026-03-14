2a00c4a3049cd009ccb43742b6f936e04b939b8d
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-03-14 00:39:38 +0000

    fix email button

--- apps/server/src/integrations/transactional/emails/invitation-email.tsx
+++ apps/server/src/integrations/transactional/emails/invitation-email.tsx
@@ -1,7 +1,7 @@
-import { Section, Text, Button } from '@react-email/components';
+import { Section, Text } from '@react-email/components';
 import * as React from 'react';
-import { button, content, paragraph } from '../css/styles';
-import { MailBody } from '../partials/partials';
+import { content, paragraph } from '../css/styles';
+import { EmailButton, MailBody } from '../partials/partials';
 
 interface Props {
   inviteLink: string;
@@ -17,19 +17,7 @@ export const InvitationEmail = ({ inviteLink }: Props) => {
           Please click the button below to accept this invitation.
         </Text>
       </Section>
-      <Section
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingLeft: '15px',
-          paddingBottom: '15px',
-        }}
-      >
-        <Button href={inviteLink} style={button}>
-          Accept Invite
-        </Button>
-      </Section>
+      <EmailButton href={inviteLink}>Accept Invite</EmailButton>
     </MailBody>
   );
 };
