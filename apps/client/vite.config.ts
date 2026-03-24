4730dc2fb95b5a50a711c107dd75fa6f2c5d818b
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-03-24 11:50:07 +0000

    cleanup

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 
-export const envPath = path.resolve(process.cwd(), "..", "..");
+const envPath = path.resolve(process.cwd(), "..", "..");
 
 export default defineConfig(({ mode }) => {
   const {
