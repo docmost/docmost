7b9a497be1500a286071c09b9b21903a88a92bf1
Ssigpoy <94902679+ssigpoy@users.noreply.github.com>
2026-04-30 16:55:54 +0800

    fix: resolve local storage path in docker

--- apps/server/src/common/helpers/constants.ts
+++ apps/server/src/common/helpers/constants.ts
@@ -3,12 +3,10 @@ import * as path from 'path';
 export const APP_DATA_PATH = 'data';
 const LOCAL_STORAGE_DIR = `${APP_DATA_PATH}/storage`;
 
-export const LOCAL_STORAGE_PATH = path.resolve(
-  process.cwd(),
-  '..',
-  '..',
-  LOCAL_STORAGE_DIR,
-);
+export const LOCAL_STORAGE_PATH =
+  path.basename(process.cwd()) === 'server'
+    ? path.resolve(process.cwd(), '..', '..', LOCAL_STORAGE_DIR)
+    : path.resolve(process.cwd(), LOCAL_STORAGE_DIR);
 
 export function getPageTitle(title: string | null | undefined): string {
   return title || 'untitled';
