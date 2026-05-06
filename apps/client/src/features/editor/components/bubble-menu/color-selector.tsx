63ae9ae6709f35afa6af521d1c2882a372df4557
dgsua <daysus@gmail.com>
2026-05-06 21:21:16 +0200

    fix: default highlight color swatch shows gray instead of white

--- apps/client/src/features/editor/components/bubble-menu/color-selector.tsx
+++ apps/client/src/features/editor/components/bubble-menu/color-selector.tsx
@@ -280,7 +280,7 @@ export const ColorSelector: FC<ColorSelectorProps> = ({
                         width: rem(28),
                         height: rem(28),
                         borderRadius: rem(4),
-                        backgroundColor: color || "var(--mantine-color-gray-2)",
+                        backgroundColor: color || "transparent",
                         border: "1px solid var(--mantine-color-gray-4)",
                         cursor: "pointer",
                         position: "relative",
