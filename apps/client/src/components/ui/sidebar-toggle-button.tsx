339b2842c42d57c910bb7773508c667e498adc19
dgsua <daysus@gmail.com>
2026-05-06 21:22:21 +0200

    fix: hamburger menu not working on Firefox for Android

--- apps/client/src/components/ui/sidebar-toggle-button.tsx
+++ apps/client/src/components/ui/sidebar-toggle-button.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import {
-  IconLayoutSidebarRightCollapse,
-  IconLayoutSidebarRightExpand
-} from "@tabler/icons-react";
-import { ActionIcon, BoxProps, ElementProps, MantineColor, MantineSize } from "@mantine/core";
+import { Burger, BurgerProps } from "@mantine/core";
 
-export interface SidebarToggleProps extends BoxProps, ElementProps<"button"> {
-  size?: MantineSize | `compact-${MantineSize}` | (string & {});
-  color?: MantineColor;
-  opened?: boolean;
-}
-
-const SidebarToggle = React.forwardRef<HTMLButtonElement, SidebarToggleProps>(
-  ({ opened, size = "sm", ...others }, ref) => {
-    return (
-      <ActionIcon size={size} {...others} variant="subtle" color="gray" ref={ref}>
-        {opened ? (
-          <IconLayoutSidebarRightExpand />
-        ) : (
-          <IconLayoutSidebarRightCollapse />
-        )}
-      </ActionIcon>
-    );
+const SidebarToggle = React.forwardRef<HTMLButtonElement, BurgerProps>(
+  ({ opened, ...others }, ref) => {
+    return <Burger opened={opened} {...others} ref={ref} />;
   }
 );
 
